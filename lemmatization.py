5ad6a4e1084de399e3e9a1438d8a00f0598f7b28
baranbingol1 <111634390+baranbingol1@users.noreply.github.com>
2023-03-22 22:19:01 +0300

    Update lemmatization.py

--- lemmatization.py
+++ lemmatization.py
@@ -13,17 +13,17 @@ def lemmetization(text):
     for word in tokens:
         if(word in hashTable):
             words.append(hashTable[word])
-            break
-        new_word = analyzer.lemmatize(word); flag = False 
-        for i in range(0,len(new_word[0][1])):
-            if new_word[0][0] == new_word[0][1][i]:
-                words.append(new_word[0][1][i])
-                hashTable[word] = new_word[0][1][i]
-                flag = True
-                break
-        if flag == True:
-            pass
         else:
-            hashTable[word] = new_word[0][1][0]
-            words.append(new_word[0][1][0])
+            new_word = analyzer.lemmatize(word); flag = False 
+            for i in range(0,len(new_word[0][1])):
+                if new_word[0][0] == new_word[0][1][i]:
+                    words.append(new_word[0][1][i])
+                    hashTable[word] = new_word[0][1][i]
+                    flag = True
+                    break
+            if flag == True:
+                pass
+            else:
+                hashTable[word] = new_word[0][1][0]
+                words.append(new_word[0][1][0])
     return ' '.join(words)
